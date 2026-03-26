0a32e7ecb25a4066594745bfb2a220eced748b98
ranum1 <rud030705@naver.com>
2026-03-26 19:45:57 +0900

    Week2

--- VS_unity2D/Assets/Scripts/Player/PlayerManager.cs
+++ VS_unity2D/Assets/Scripts/Player/PlayerManager.cs
@@ -1,34 +1,45 @@
+using NUnit.Framework;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
 public class PlayerManager : MonoBehaviour
 {
-
     PlayerInput playerInput;
+    SpriteRenderer spriter;
     Rigidbody2D rb;
     Vector2 moveVec;
-    public float speed = 4f;
 
+    Animator anim;
+    public float speed = 4f;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         playerInput = GetComponent<PlayerInput>();
         rb = GetComponent<Rigidbody2D>();
+        spriter = GetComponent<SpriteRenderer>();
+        anim = GetComponent<Animator>();
     }
 
     // Update is called once per frame
     private void Update()
     {
-        // ½ÇÁ¦ ÀÌµ¿ ·ÎÁ÷Àº Update³ª FixedUpdate¿¡¼­ moveInputÀ» »ç¿ëÇØ Ã³¸®ÇÕ´Ï´Ù.
+        // ì‹¤ì œ ì´ë™ ë¡œì§ì€ Updateë‚˜ FixedUpdateì—ì„œ moveInputì„ ì‚¬ìš©í•´ ì²˜ë¦¬í•©ë‹ˆë‹¤.
         // 
         rb.linearVelocity = new Vector2(moveVec.x * speed, moveVec.y * speed);
+        spriter.flipX = (moveVec.x > 0 ? false : true);
     }
 
     public void OnMove(InputAction.CallbackContext context)
     {
         moveVec = context.ReadValue<Vector2>();
-
-     
+        if (context.phase == InputActionPhase.Performed)
+        {
+            anim.SetBool("isRun", true);
+        }
+        else if (context.phase == InputActionPhase.Canceled)
+        {
+            anim.SetBool("isRun", false);
+        }
     }
 }
