66e72c0a3c153ee2900277d9250060f3e8667e64
penguinjean0421 <rud030705@naver.com>
2026-04-06 20:58:03 +0900

    Week4-1
    몬스터 만들기

---
+++ VS_unity2D/Assets/Scripts/Enemy.cs
@@ -0,0 +1,34 @@
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    public float speed;
+    public Rigidbody2D target;
+    bool isLive = true;
+
+    Rigidbody2D rigid;
+    SpriteRenderer spriter;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Awake()
+    {
+        rigid = GetComponent<Rigidbody2D>();
+        spriter = GetComponent<SpriteRenderer>();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        if (!isLive)
+            return;
+
+        Vector2 dirVec = target.position - rigid.position;
+        Vector2 nextVec = dirVec.normalized * speed * Time.fixedDeltaTime;
+        rigid.MovePosition(rigid.position + nextVec);
+        rigid.linearVelocity = Vector2.zero;
+    }
+
+    void LateUpdate()
+    {
+        spriter.flipX = target.position.x < rigid.position.x;
+    }
+}
--- VS_unity2D/Assets/Scripts/Reposition.cs
+++ VS_unity2D/Assets/Scripts/Reposition.cs
@@ -1,10 +1,16 @@
-using System;
 using System.Transactions;
 using Unity.VisualScripting;
+using UnityEditorInternal;
 using UnityEngine;
 
 public class Reposition : MonoBehaviour
 {
+    Collider2D coll;
+
+    void Awake()
+    {
+        coll = GetComponent<Collider2D>();
+    }
     void OnTriggerExit2D(Collider2D collision)
     {
         if (!collision.CompareTag("Area"))
@@ -29,6 +35,13 @@ public class Reposition : MonoBehaviour
                 else if (diffX < diffY)
                 {
                     transform.Translate(Vector3.up * dirY * 40);
+                }
+                break;
+
+            case "Enemy":
+                if (coll.enabled)
+                {
+                    transform.Translate(playerDir * 20 + new Vector3(Random.Range(-3f, 3f), Random.Range(-3f, 3f), 0f));
                 }
                 break;
         }
